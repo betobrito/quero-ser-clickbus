3480f12415ea9777fcbba0b30a5d5a7aea3069ca
betobrito <Beto0529!>
2019-09-24 00:06:15 -0300

    Added liquibase to project, added circleci to it, added surefire reports

--- testes/backend-developer/solution/places/src/main/java/br/com/clickbus/places/PlacesApplication.java
+++ testes/backend-developer/solution/places/src/main/java/br/com/clickbus/places/PlacesApplication.java
@@ -2,8 +2,11 @@ package br.com.clickbus.places;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({LiquibaseProperties.class})
 public class PlacesApplication {
 
     public static void main(String[] args) {
